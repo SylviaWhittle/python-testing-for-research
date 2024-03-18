fff1bf3980d9480d4ae744fca05d4dc3decaf9d9
SylviaWhittle <sylviwhittle@gmail.com>
2024-03-18 01:00:50 +0000

    Add start of simple-tests lesson

--- episodes/files/scripts.py
+++ episodes/files/scripts.py
@@ -8,3 +8,12 @@ def broken_check_if_prime(n):
         if n % i == 0:
             return False
     return True
+
+def check_if_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
